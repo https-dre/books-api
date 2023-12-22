4c1a8b4cb271c7b62c99384ea05286945abca194
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-22 20:35:18 -0300

    feat (CreateBookController.ts): Adiciona método handle ao controller createBook.

--- src/useCases/createBook/createBookController.ts
+++ src/useCases/createBook/createBookController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 import { Book } from '../../models/Book';
-import { CreateBookUseCase } from "./createBookUseCase";
+import { CreateBookUseCase } from "./CreateBookUseCase";
 
 export class CreateBookController {
     private createBookCase : CreateBookUseCase
@@ -8,5 +8,26 @@ export class CreateBookController {
     constructor( bookCase : CreateBookUseCase)
     {
         this.createBookCase = bookCase
+    }
+
+    async handle(request : Request, response : Response) : Promise<Response>
+    {
+        try {
+            const { name, autor } = request.body
+            let filePath = request.file?.fieldname + '-' + name.replace(' ', '_') || null
+            const result = await this.createBookCase.execute({
+                name: name,
+                autor: autor,
+                epubPath: filePath
+            })
+            if(result)
+            {
+                return response.status(201).json(result)
+            }
+            return response.status(400).send('Erro ao cadastrar Livro')
+
+        } catch (error) {
+            return response.status(500).send('Erro interno no Servidor.')
+        }
     }
 }
