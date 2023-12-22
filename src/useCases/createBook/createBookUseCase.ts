ad6dcb1bb4677a928a53ee4bef249441f95fec0c
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-22 19:01:23 -0300

    feat (createBookUseCase.ts): Adiciona UseCase do createBook.

---
+++ src/useCases/createBook/createBookUseCase.ts
@@ -0,0 +1,33 @@
+import { Book } from "../../models/Book"
+import { IBookRepository } from "../../repositories/protocols/IBookRepository"
+import { v4 as uuid} from "uuid"
+
+
+export class CreateBookUseCase {
+	private bookRepository : IBookRepository;
+	constructor(repository : IBookRepository)
+	{
+		this.bookRepository = repository
+	}
+
+	async execute(book : Omit<Book,'id'>) : Promise<Book | null>
+	{
+		const bookFinded = await this.bookRepository.findByName(book.name);
+		if(bookFinded)
+		{
+			throw new Error('O usuário já Existe')
+		}
+		
+		const bookToSave : Book = {
+			id: uuid(),
+            name: book.name,
+        	autor: book.autor,
+        	epubPath: book.epubPath
+		}
+
+
+		await this.bookRepository.save(bookToSave)
+
+		return bookToSave
+	}
+}
