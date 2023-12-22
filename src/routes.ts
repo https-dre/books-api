1fdca9dfe1c0abe13239c56db3ab8ab9cc1db806
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2023-12-22 20:33:46 -0300

    test (routes.ts): Adiciona rota de publicação de Livros.

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,23 @@
 import { Router } from 'express'
+import { upload } from './config/multerConfig'
+
+
+import { BookRepository } from './repositories/implementations/BookRepository'
+import { CreateBookUseCase } from './useCases/createBook/CreateBookUseCase'
+import { CreateBookController } from './useCases/createBook/CreateBookController'
 
 const router = Router()
 
 router.get('/', (req, res) => {
 	res.send('Hello Word')
+})
+
+router.post('/', upload.single('epub'), (req, res)=>{
+	const bookRepo = new BookRepository()
+	const bookUseCase = new CreateBookUseCase(bookRepo)
+	const bookController = new CreateBookController(bookUseCase)
+
+	return bookController.handle(req, res)
 })
 
 export { router }
