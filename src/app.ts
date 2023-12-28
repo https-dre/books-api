1464f8b87aebd31c1a02460f5b291b90b464c3e7
DiasoAndre <diaso.andre@outlook.com>
2023-12-28 15:41:39 -0300

    feat (app.ts): Adiciona cabeçalho cors.

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,10 @@ app.use(express.json())
 
 app.use(router)
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', "*");
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+});
+
 export { app }
