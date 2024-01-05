a10ec7960512c6f28651ac03c7be30e2a05777e1
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-05 15:03:01 -0300

    refactor (createBookUseCase.ts): Retira a verificação se existe um livro com o nome.

--- src/useCases/createBook/createBookUseCase.ts
+++ src/useCases/createBook/createBookUseCase.ts
@@ -2,7 +2,6 @@ import { Book } from "../../models/Book"
 import { IBookRepository } from "../../repositories/protocols/IBookRepository"
 import { v4 as uuid} from "uuid"
 
-
 export class CreateBookUseCase {
 	private bookRepository : IBookRepository;
 	constructor(repository : IBookRepository)
@@ -12,20 +11,12 @@ export class CreateBookUseCase {
 
 	async execute(book : Omit<Book,'id'>) : Promise<Book | null>
 	{
-		const bookFinded = await this.bookRepository.findByName(book.name);
-		if(bookFinded)
-		{
-			throw new Error('O usuário já Existe')
-		}
-		
 		const bookToSave : Book = {
 			id: uuid(),
             name: book.name,
         	autor: book.autor,
         	file: book.file
-		}
-
-
+		} 
 		await this.bookRepository.save(bookToSave)
 
 		return bookToSave
