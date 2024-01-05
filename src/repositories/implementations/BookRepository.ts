4dff8d431511a12fa666a87351c484a98076cca4
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-05 14:35:38 -0300

    feat (BookRepository.ts): Adiciona métodos do PostgreSQL.

--- src/repositories/implementations/BookRepository.ts
+++ src/repositories/implementations/BookRepository.ts
@@ -1,15 +1,38 @@
 import { IBookRepository } from "../protocols/IBookRepository";
 import { Book } from "../../models/Book";
+import { dbPool } from "../../data/db-postgres";
 
 export class BookRepository implements IBookRepository
 {
     async save(book: Book): Promise<void> {
-        
-    }
-    async findByName(name: string): Promise<Book | null> {
         try {
-            return null
+            const db = await dbPool.connect()
+
+            const { rows } = await db.query(`
+                INSERT INTO books (id, name, autor, file) VALUES ( $1, $2, $3, $4)
+            `, [book.id, book.name, book.autor, book.file])
+
+            await db.release()
+
         } catch (error) {
+            console.log(error)
+            throw new Error('Erro ao Salvar Livro')
+        }
+    }
+    async findByName(name: string): Promise<Book[]> {
+        try {
+            const db = await dbPool.connect()
+
+            const result = await db.query(`
+                SELECT * FROM books WHERE name = $1
+            `,[name])
+
+            const rows : Book[] = result.rows
+
+            return rows
+            
+        } catch (error) {
+            console.log(error)
             throw new Error('Erro no BookRepository')
         }
     }
