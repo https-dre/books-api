356c9c22599495a4c097cde037771af717bf4223
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-05 16:02:02 -0300

    refactor (db-postgres.ts): Cria objeto data com método de configuração.

--- src/data/db-postgres.ts
+++ src/data/db-postgres.ts
@@ -1,18 +1,22 @@
-import { Pool, PoolClient } from "pg";
+import { Pool } from "pg";
 import * as dotenv from 'dotenv'
 
-dotenv.config()
+export const data = {
+    dbPool : undefined as unknown as Pool,
+    async conn() : Promise<void>
+    {
+        dotenv.config()
 
-export const dbPool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DATABASE,
-    password : process.env.DB_PASSW as unknown as string,
-    port : process.env.DB_PORT as unknown as number
-})
+        this.dbPool = new Pool({
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DATABASE,
+            password : process.env.DB_PASSW as unknown as string,
+            port : process.env.DB_PORT as unknown as number
+        })
 
-export const db_con = async () => {
-    const client = await dbPool.connect()
-    client.release()
+        console.log('PostgreSQL autenticado com Sucesso!!')
+    }
 }
+
 
