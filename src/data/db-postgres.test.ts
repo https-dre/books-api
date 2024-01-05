bb8d613c4c4ceac44f26cb0a8557bd63a52fc13f
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-05 16:03:14 -0300

    refactor (db-postgres.test.ts): Adiciona objeto data.

--- src/data/db-postgres.test.ts
+++ src/data/db-postgres.test.ts
@@ -1,15 +1,15 @@
-import { dbPool } from "./db-postgres"
+import { data } from "./db-postgres"
 import { expect, test } from 'vitest'
 import { QueryResult } from "pg"
 import { v4 as uuid } from 'uuid';
 
 test('Conectar ao Banco de Dados', async () => {
-    const client = await dbPool.connect()
+    const client = await data.dbPool.connect()
     client.release()
 })
 
 test('Realizar uma Query', async () => {
-    const client = await dbPool.connect()
+    const client = await data.dbPool.connect()
 
     const result = await client.query('SELECT * FROM books LIMIT 100')
 
@@ -17,7 +17,7 @@ test('Realizar uma Query', async () => {
 })
 
 test('Insert no PostgreSQL', async () => {
-    const client = await dbPool.connect()
+    const client = await data.dbPool.connect()
 
     const result = await client.query(`INSERT INTO books (id, name, autor, file)
     VALUES ($1, $2, $3, $4)`,
