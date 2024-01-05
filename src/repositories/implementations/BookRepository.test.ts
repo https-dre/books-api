e2c76a03346c58f6230e73206a0d224a7173fb92
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-05 14:59:48 -0300

    feat (BookRepository.test.ts): Cria arquivo de testes do BookRepository.

---
+++ src/repositories/implementations/BookRepository.test.ts
@@ -0,0 +1,26 @@
+import { dbPool } from "../../data/db-postgres"
+import { expect, test } from 'vitest'
+import { BookRepository } from './BookRepository';
+import { Book } from '../../models/Book';
+import { v4 as uuid} from "uuid"
+
+test('Tentando Criar Livro', async () => {
+    const book : Book = {
+        id: uuid(),
+        name: "O Código do Milhão",
+        autor: "Pablo Marçal",
+        file: "null"
+    }
+    const bookRepository = new BookRepository()
+
+    await bookRepository.save(book)
+})
+
+test('Pesquisando Livros', async () => {
+    const bookRepository = new BookRepository()
+
+    const books = await bookRepository.findByName('O Código Do Milhão')
+
+    console.log(books)
+
+})
