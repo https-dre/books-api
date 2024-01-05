6b50a501317cf36339bf0027dfceff0b95441d33
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-05 15:29:07 -0300

    feat (GetBookController.ts): Adiciona controller GetBook.

---
+++ src/useCases/getBook/GetBookController.ts
@@ -0,0 +1,27 @@
+import { GetBookUseCase } from "./GetBookUseCase";
+import { Request, Response } from 'express';
+
+
+export class GetBookController {
+    private getBookUseCase : GetBookUseCase
+
+    constructor(getBook : GetBookUseCase)
+    {
+        this.getBookUseCase = getBook
+    }
+
+    async handle(request : Request, response : Response) : Promise<Response>
+    {
+        console.log('Chegou no controller')
+        const { name } = request.params
+
+        try {
+            const result = await this.getBookUseCase.execute(name)
+
+            return response.status(result.status).json(result.body)
+        } catch (error : any) {
+            console.log(error)
+            return response.status(500).json({message : error.message || 'Unexpected Error.'})
+        }
+    }
+}
