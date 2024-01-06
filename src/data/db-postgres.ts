78ad04b27ac9fa3ad9e5f5b8be4841a21a6c3acd
DiasoAndre <136815797+DiasoAndre@users.noreply.github.com>
2024-01-06 18:05:55 -0300

    refactor (db-postgres.ts): Tratando erro com PostgreSQL.

--- src/data/db-postgres.ts
+++ src/data/db-postgres.ts
@@ -15,7 +15,16 @@ export const data = {
             port : process.env.DB_PORT as unknown as number
         })
 
-        console.log('PostgreSQL autenticado com Sucesso!!')
+        try {
+            const client = await this.dbPool.connect()
+            client.release()
+            console.log('PostgreSQL autenticado com Sucesso!!')
+        } catch (error) {
+            console.log(`Erro ao autenticar com PostgreSQL.`)
+            console.log(`\taddress:${process.env.DB_HOST}\n\tport:${process.env.DB_PORT}`)
+            console.log(`\tuser:${process.env.DB_USER}\n\tpassword:${process.env.DB_PASSW}`)
+            throw error
+        }   
     }
 }
 
